79d26deaef50b88f3d4c16c09b7f2c5fd8169df5
nabzali <nabeelali_@outlook.com>
2023-12-25 00:09:29 +0000

    a lot of changes

--- README.md
+++
@@ -1,1 +0,0 @@
-# nabzali.github.io
---
+++ script.js
@@ -0,0 +1,18 @@
+var jobTitleText = new Typed(".dynamic-text", {
+  strings: [
+    "Full-Stack Developer",
+    "Technical Consultant",
+    "Front-end Developer",
+    "Back-end Developer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 500,
+  loop: true,
+});
+
+var terminalText = new Typed(".terminal-text", {
+  strings: ["Are you ready? Press Enter to continue..."],
+  typeSpeed: 100,
+  backSpeed: 100,
+});
